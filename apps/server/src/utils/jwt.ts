6db8ee3989868cbf489031bf901ca95170677b7a
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-11-28 12:53:05 +0000

    feat(server): add jwt utils

---
+++ apps/server/src/utils/jwt.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { User } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import { prisma } from "../database/prisma";
+import { jwtSchema } from "../schemas/jwt";
+
+const generateUserToken = (user: User) => {
+  const token = jwt.sign(user.id, process.env.JWT_SECRET!);
+
+  return `Bearer ${token}`;
+};
+
+const getUserFromToken = async (rawToken: string) => {
+  const [bearer, token] = rawToken.split("Bearer ");
+
+  if (!token)
+    throw new TRPCError({ code: "BAD_REQUEST", message: "Invalid JWT" });
+
+  const userId = jwtSchema.parse(token);
+
+  const user = await prisma.user.findUniqueOrThrow({ where: { id: userId } });
+  return user;
+};
