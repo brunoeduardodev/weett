ff0553734d35bb21f39f8e2c0e01b46553550d78
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-11-28 10:39:14 +0000

    feat: initial structure

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "prettier/prettier": "error",
   },
 };
