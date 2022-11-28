03866b91127fd886fc904e8e36b8099768aed1ea
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-11-28 18:56:09 +0000

    feat(server): add login route

--- apps/server/src/routers/authentication.ts
+++ apps/server/src/routers/authentication.ts
@@ -1,9 +1,14 @@
-import { userSignupSchema } from "../schemas/authentication";
+import { userLoginSchema, userSignupSchema } from "../schemas/authentication";
+import { login } from "../services/authentication/login";
 import { signup } from "../services/authentication/signup";
 import { t } from "../trpc";
 
 export const authenticationRouter = t.router({
   userSignup: t.procedure.input(userSignupSchema).mutation(({ input, ctx }) => {
     return signup(input, ctx);
+  }),
+
+  userLogin: t.procedure.input(userLoginSchema).mutation(({ input, ctx }) => {
+    return login(input, ctx);
   }),
 });
