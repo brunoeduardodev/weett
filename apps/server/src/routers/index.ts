8af799d47438262e4abd116dc2e72b2d3a9c2000
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-11-30 09:39:08 +0000

    feat(server): add nextauth required handlers

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -3,9 +3,10 @@ import { authenticationRouter } from "./authentication";
 import { feedRouter } from "./feed";
 import { healthRouter } from "./health";
 
-export const appRouter = t.mergeRouters(
-  healthRouter,
-  authenticationRouter,
-  feedRouter
-);
+export const appRouter = t.router({
+  auth: authenticationRouter,
+  feed: feedRouter,
+  health: healthRouter,
+});
+
 export type AppRouter = typeof appRouter;
--- packages/schemas/authentication.ts
+++ packages/schemas/authentication.ts
@@ -1,17 +1,90 @@
 import { z } from "zod";
 
-export const userSignupSchema = z.object({
-  name: z.string().min(3),
-  handle: z.string().min(3),
+export const createUserSchema = z.object({
+  name: z.string().optional(),
   email: z.string().email(),
-  password: z.string().min(6),
+  image: z.string().optional(),
 });
 
-export type UserSignupInput = z.infer<typeof userSignupSchema>;
+export type CreateUserInput = z.infer<typeof createUserSchema>;
 
-export const userLoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+export const getUserSchema = z.object({
+  id: z.string().cuid(),
 });
 
-export type UserLoginInput = z.infer<typeof userLoginSchema>;
+export type GetUserInput = z.infer<typeof getUserSchema>;
+
+export const getUserByEmailSchema = z.object({
+  email: z.string().email(),
+});
+
+export type GetUserByEmailInput = z.infer<typeof getUserSchema>;
+
+export const getUserByAccountSchema = z.object({
+  provider_providerAccountId: z.object({
+    provider: z.string(),
+    providerAccountId: z.string(),
+  }),
+});
+
+export type GetUserByAccountInput = z.infer<typeof getUserByAccountSchema>;
+
+export const updateUserSchema = z.object({
+  id: z.string().cuid(),
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  image: z.string().optional(),
+});
+
+export type UpdateUserInput = z.infer<typeof updateUserSchema>;
+
+export const deleteUserSchema = z.object({
+  id: z.string().cuid(),
+});
+
+export type DeleteUserInput = z.infer<typeof deleteUserSchema>;
+
+export const linkAccountSchema = z.object({
+  userId: z.string().cuid(),
+  type: z.string(),
+  provider: z.string(),
+  providerAccountId: z.string(),
+});
+
+export type LinkAccountInput = z.infer<typeof linkAccountSchema>;
+
+export const unlinkAccountSchema = z.object({
+  provider_providerAccountId: z.object({
+    provider: z.string(),
+    providerAccountId: z.string(),
+  }),
+});
+
+export type UnlinkAccountInput = z.infer<typeof unlinkAccountSchema>;
+
+export const getSessionAndUserSchema = z.object({
+  sessionToken: z.string(),
+});
+
+export type GetSessionAndUserInput = z.infer<typeof getSessionAndUserSchema>;
+
+export const createSessionSchema = z.object({
+  sessionToken: z.string(),
+  userId: z.string().cuid(),
+  expires: z.date(),
+});
+
+export type CreateSessionInput = z.infer<typeof createSessionSchema>;
+
+export const updateSessionSchema = z.object({
+  sessionToken: z.string(),
+  expires: z.date(),
+});
+
+export type UpdateSessionInput = z.infer<typeof updateSessionSchema>;
+
+export const deleteSessionSchema = z.object({
+  sessionToken: z.string(),
+});
+
+export type DeleteSessionInput = z.infer<typeof deleteSessionSchema>;
