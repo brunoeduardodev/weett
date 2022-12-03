49ecbf58233d5ee25d40e14bda032056a1b154aa
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-03 17:19:59 +0000

    feat(server): add initial upload structure

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,16 +1,33 @@
+import multer from "multer";
 import Express from "express";
 import cors from "cors";
+
+const upload = multer({ dest: "uploads/" });
 
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { appRouter, createContext } from "./trpc";
 
 const app = Express();
 app.use(cors());
+
 app.use("/trpc", createExpressMiddleware({ router: appRouter, createContext }));
 
 app.use("/", (req, res) => {
   res.send("Hello world");
 });
+
+app.post("/uploads/profile-picture", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded");
+  }
+
+  if (!req.file.mimetype.startsWith("image/")) {
+    return res.status(400).send("File is not an image");
+  }
+
+  return req.file.destination;
+});
+
 app.listen(4000);
 
 export type AppRouter = typeof appRouter;
