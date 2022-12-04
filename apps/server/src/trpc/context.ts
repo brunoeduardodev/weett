d292f86817e48c5deca7d9c85aa9e17e650dfd67
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-04 15:38:27 +0000

    feat(server): separate context logic

---
+++ apps/server/src/trpc/context.ts
@@ -0,0 +1,25 @@
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { prisma } from "@/database/prisma";
+import { getUserFromToken } from "@/utils/jwt";
+import { User } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
+
+export const createContext = async ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  const token = req.headers.authorization;
+  if (!token)
+    return {
+      prisma,
+    };
+
+  const user = await getUserFromToken(token);
+  return {
+    user,
+    prisma,
+  };
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
+export type AuthorizedContext = Context & { user: User };
--- apps/server/src/trpc/index.ts
+++ apps/server/src/trpc/index.ts
@@ -1,31 +1,8 @@
 import SuperJSON from "superjson";
-import { inferAsyncReturnType, initTRPC } from "@trpc/server";
-import * as trpcExpress from "@trpc/server/adapters/express";
-import { prisma } from "@/database/prisma";
-import { getUserFromToken } from "@/utils/jwt";
-import { User } from "@prisma/client";
-
-export const createContext = async ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => {
-  const token = req.headers.authorization;
-  if (!token)
-    return {
-      prisma,
-    };
-
-  const user = await getUserFromToken(token);
-  return {
-    user,
-    prisma,
-  };
-};
-
-export type Context = inferAsyncReturnType<typeof createContext>;
-export type AuthorizedContext = Context & { user: User };
+import { initTRPC } from "@trpc/server";
+import { Context } from "./context";
 
 export const t = initTRPC.context<Context>().create({ transformer: SuperJSON });
 
-export { appRouter } from "./routers";
-export type { AppRouter } from "./routers";
+export * from "./context";
+export * from "./routers";
