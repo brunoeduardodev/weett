88413349eac8d0216669ea01f7b5988e6c784fd7
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-05 23:24:07 +0000

    refactor(server): move post to separated service folder

--- apps/server/src/trpc/routers/feed.ts
+++ apps/server/src/trpc/routers/feed.ts
@@ -1,7 +1,8 @@
-import { getFeed, createPost } from "../services/feed";
+import { getFeed } from "../services/feed";
 import { t } from "..";
 import { createPostSchema } from "@weett/schemas";
 import { ensureAuthenticated } from "../middlewares";
+import { createPost } from "../services/post";
 
 export const feedRouter = t.router({
   createPost: t.procedure
--- apps/server/src/trpc/services/feed/index.ts
+++ apps/server/src/trpc/services/feed/index.ts
@@ -1,2 +1,1 @@
 export * from "./getFeed";
-export * from "./createPost";
