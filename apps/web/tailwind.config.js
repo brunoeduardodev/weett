d064ca9fa1224bd9f86a2125263ea98028f1bc41
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-05 15:06:56 +0000

    chore: add shared tailwind config package

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,18 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./src/**/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        "modal-open": "modal-open 0.3s ease-in-out",
-      },
-      keyframes: {
-        "modal-open": {
-          "0%": { opacity: 0, transform: "translate(-50%, -48%) scale(.96)" },
-          "100%": { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
-        },
-      },
-    },
-  },
-  plugins: [],
+  presets: [require("@weett/tailwind")],
 };
