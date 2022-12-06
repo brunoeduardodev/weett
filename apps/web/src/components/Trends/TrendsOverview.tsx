9fdb494c34b04c31f0f6a08e056c71f67989e066
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-06 12:19:35 +0000

    feat: add trending overview box

---
+++ apps/web/src/components/Trends/TrendsOverview.tsx
@@ -0,0 +1,7 @@
+export const TrendsOverview = () => {
+  return (
+    <section className="flex flex-col bg-gray-100 h-[420px] w-full items-center justify-center">
+      <p className="text-lg ">Coming Soon</p>
+    </section>
+  );
+};
--- apps/web/src/layouts/main.tsx
+++ apps/web/src/layouts/main.tsx
@@ -1,11 +1,18 @@
 import { PropsWithChildren } from "react";
 import { Sidebar } from "@/components/Sidebar";
+import { TrendsOverview } from "@/components/Trends/TrendsOverview";
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex flex-col p-4 pl-[320px] flex-1">{children}</div>
+      <div className="flex flex-col p-4 pl-[320px] xl:pr-[320px] w-full">
+        {children}
+      </div>
+      <div className="hidden xl:flex flex-col p-4 w-[320px] min-h-screen fixed right-0 top-0">
+        <h3 className="text-2xl font-bold mb-4">Trending</h3>
+        <TrendsOverview />
+      </div>
     </div>
   );
 };
