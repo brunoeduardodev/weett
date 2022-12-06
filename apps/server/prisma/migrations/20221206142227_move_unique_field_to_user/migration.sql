49d51609dc35aebdf515e643b753f9b0d18f30aa
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-06 14:22:35 +0000

    feat: move unique constraint to user model
    doing this to prevent user.profile being optional,

---
+++ apps/server/prisma/migrations/20221206142227_move_unique_field_to_user/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `Profile` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[profileId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `profileId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- DropIndex
+DROP INDEX "Profile_userId_key";
+
+-- AlterTable
+ALTER TABLE "Profile" DROP COLUMN "userId";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "profileId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_profileId_key" ON "User"("profileId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
