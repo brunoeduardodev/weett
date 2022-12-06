08068d7132bc9741c08bffebebe12193e2413065
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-06 11:55:18 +0000

    feat: makes sidebar item activate dynamically

--- apps/web/src/components/Sidebar/index.tsx
+++ apps/web/src/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import { useAuthentication } from "@/contexts/authentication";
 import { useAuthenticationDialog } from "@/contexts/authenticationDialog";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 import { AuthDialog } from "../AuthDialog";
 import { AccountMenu } from "./AccountMenu";
 import { AuthenticationZone } from "./AuthenticationZone";
@@ -9,20 +11,29 @@ export const Sidebar = () => {
   const { user } = useAuthentication();
   const { showLogin, showRegister } = useAuthenticationDialog();
 
+  const router = useRouter();
+
+  const isItemActive = useCallback(
+    (href: string) => {
+      return router.pathname === href;
+    },
+    [router.pathname]
+  );
+
   return (
     <div className="flex flex-col h-screen fixed w-[320px] justify-between p-4">
       <ul className="flex flex-col gap-2 items-start">
         <h1 className="px-4 font-bold text-xl mb-6">Something Cool</h1>
 
-        <SidebarItem href={"/"} isActive={true}>
+        <SidebarItem href={"/"} isActive={isItemActive("/")}>
           Feed
         </SidebarItem>
-        <SidebarItem href={"/trending"} isActive={false}>
+        <SidebarItem href={"/trending"} isActive={isItemActive("/trending")}>
           Trending
         </SidebarItem>
 
         {user && (
-          <SidebarItem href={"/me"} isActive={false}>
+          <SidebarItem href={"/me"} isActive={isItemActive("/me")}>
             Profile
           </SidebarItem>
         )}
