c115b65ae25ecdd62a2b17c31cb0dcf16035c2f9
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-07 15:23:18 +0000

    feat: add update self router

--- apps/server/src/trpc/routers/user.ts
+++ apps/server/src/trpc/routers/user.ts
@@ -1,6 +1,13 @@
 import { me } from "../services/user/me";
 import { t } from "..";
+import { ensureAuthenticated } from "../middlewares";
+import { updateSelf } from "../services/user/updateSelf";
+import { updateSelfSchema } from "@weett/schemas";
 
 export const userRouter = t.router({
   me,
+  updateSelf: t.procedure
+    .use(ensureAuthenticated)
+    .input(updateSelfSchema)
+    .mutation(({ input, ctx }) => updateSelf(input, ctx)),
 });
