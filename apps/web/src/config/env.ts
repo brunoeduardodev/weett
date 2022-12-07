762d75a73434df717af513ef94fdec07df90fb1c
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-07 18:16:03 +0000

    feat: add upload url env

--- apps/web/src/config/env.ts
+++ apps/web/src/config/env.ts
@@ -2,10 +2,12 @@ import { z } from "zod";
 
 const schema = z.object({
   trpcUrl: z.string().url(),
+  uploadUrl: z.string().url(),
 });
 
 const envParse = schema.safeParse({
   trpcUrl: process.env.NEXT_PUBLIC_TRPC_URL,
+  uploadUrl: process.env.NEXT_PUBLIC_UPLOAD_URL,
 });
 
 if (!envParse.success) {
