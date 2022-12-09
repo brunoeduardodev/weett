5b4ffed578c77ebc47edd5148d37e80b28081086
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-09 23:32:49 +0000

    feat: add like route

--- apps/server/src/trpc/routers/post.ts
+++ apps/server/src/trpc/routers/post.ts
@@ -1,11 +1,16 @@
-import { createPostSchema } from "@weett/schemas";
+import { createPostSchema, likePostSchema } from "@weett/schemas";
 import { t } from "..";
 import { ensureAuthenticated } from "../middlewares";
 import { createPost } from "../services/post";
+import { likePost } from "../services/post/likePost";
 
 export const postRouter = t.router({
   create: t.procedure
     .input(createPostSchema)
     .use(ensureAuthenticated)
     .mutation(({ input, ctx }) => createPost(input, ctx)),
+  like: t.procedure
+    .input(likePostSchema)
+    .use(ensureAuthenticated)
+    .mutation(({ input, ctx }) => likePost(input, ctx)),
 });
