26c99ca95fa3dd85a68e65e101dd0966d90e7a4d
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-09 23:24:32 +0000

    fix: dialog zIndex

--- apps/web/src/components/Profile/EditProfileDialog/index.tsx
+++ apps/web/src/components/Profile/EditProfileDialog/index.tsx
@@ -3,7 +3,7 @@ import { useZodForm } from "@/hooks/useZodForm";
 import { trpc } from "@/utils/trpc";
 import * as Dialog from "@radix-ui/react-dialog";
 import { UpdateSelfInput, updateSelfSchema } from "@weett/schemas";
-import { Button, ImageField, TextField } from "@weett/ui";
+import { Button, TextField } from "@weett/ui";
 import { useCallback, useEffect } from "react";
 import { Controller } from "react-hook-form";
 
@@ -40,7 +40,13 @@ export const EditProfileDialog = ({ isOpen, onClose }: Props) => {
       avatarUrl: userQuery.data?.profile.avatarUrl ?? undefined,
       bannerUrl: userQuery.data?.profile.bannerUrl ?? undefined,
     });
-  }, [userQuery.data?.profile.name, userQuery.data?.profile.bio, reset]);
+  }, [
+    userQuery.data?.profile.name,
+    userQuery.data?.profile.bio,
+    userQuery.data?.profile.avatarUrl,
+    userQuery?.data?.profile.bannerUrl,
+    reset,
+  ]);
 
   useEffect(() => {
     handleReset();
@@ -52,8 +58,8 @@ export const EditProfileDialog = ({ isOpen, onClose }: Props) => {
 
   return (
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
-      <Dialog.DialogOverlay className="bg-black bg-opacity-25 fixed inset-0 transition-colors ease-in-out duration-300" />
-      <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-lg p-6 animate-modal-open flex flex-col">
+      <Dialog.DialogOverlay className="bg-black bg-opacity-25 fixed inset-0 transition-colors ease-in-out duration-300 z-10" />
+      <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 z-10 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-lg p-6 animate-modal-open flex flex-col">
         <Dialog.Title className="font-bold text-lg text-center">
           Edit Profile
         </Dialog.Title>
