8c8338e0d5da6d63cdf8e19a277d4f5946b9863f
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-27 22:34:17 +0000

    fix: dialog z indexes

--- apps/web/src/components/AuthDialog/index.tsx
+++ apps/web/src/components/AuthDialog/index.tsx
@@ -10,7 +10,7 @@ export const AuthDialog = () => {
   return (
     <Dialog.Root open={isOpen} onOpenChange={close}>
       <Dialog.DialogOverlay className="bg-black bg-opacity-25 fixed inset-0 transition-colors ease-in-out duration-300 z-50" />
-      <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-lg p-6 animate-modal-open flex flex-col">
+      <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-lg p-6 animate-modal-open flex flex-col z-50">
         <Dialog.Title className="font-bold text-lg text-center">
           {currentMethod === "login" ? "Sign In" : "Sign Up"}
         </Dialog.Title>
--- apps/web/src/components/Profile/EditProfileDialog/index.tsx
+++ apps/web/src/components/Profile/EditProfileDialog/index.tsx
@@ -58,8 +58,8 @@ export const EditProfileDialog = ({ isOpen, onClose }: Props) => {
 
   return (
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
-      <Dialog.DialogOverlay className="bg-black bg-opacity-25 fixed inset-0 transition-colors ease-in-out duration-300 z-10" />
-      <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 z-10 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-lg p-6 animate-modal-open flex flex-col">
+      <Dialog.DialogOverlay className="bg-black bg-opacity-25 fixed inset-0 transition-colors ease-in-out duration-300 z-50" />
+      <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-lg p-6 animate-modal-open flex flex-col z-50">
         <Dialog.Title className="font-bold text-lg text-center">
           Edit Profile
         </Dialog.Title>
