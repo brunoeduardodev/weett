9fa2e858a754d69b10838876c14e3ec69b6b8b68
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-31 14:10:07 +0000

    feat: make post author name and image clickable

--- apps/web/src/components/Feed/Post/index.tsx
+++ apps/web/src/components/Feed/Post/index.tsx
@@ -14,6 +14,7 @@ import { trpc } from "@/utils/trpc";
 import { useAuthentication } from "@/contexts/authentication";
 import { useAuthenticationDialog } from "@/contexts/authenticationDialog";
 import { Handle } from "@/components/Handle";
+import Link from "next/link";
 
 type Props = {
   post: inferRouterOutputs<AppRouter>["feed"]["get"]["posts"][0];
@@ -57,17 +58,26 @@ export const Post = ({ post }: Props) => {
 
   return (
     <article className="p-4 flex gap-4 items-start ">
-      <div className="">
+      <Link
+        href={`/${post.author.handle}`}
+        aria-label={post.author.profile.name}
+        className="bg-black rounded-full"
+      >
         <img
-          className="w-12 h-12 rounded-full bg-gray-400 object-cover"
+          className="w-12 h-12 rounded-full bg-gray-400 filter hover:opacity-75 transition"
           alt={`${post.author.profile.name}'s avatar`}
           src={post.author.profile.avatarUrl || undefined}
         />
-      </div>
+      </Link>
 
       <div className="flex flex-col gap-3 flex-1">
         <div className="flex items-center gap-4">
-          <h3 className="font-bold">{post.author.profile.name}</h3>
+          <Link
+            href={`/${post.author.handle}`}
+            className="font-bold hover:underline"
+          >
+            {post.author.profile.name}
+          </Link>
           <Handle handle={post.author.handle} />
           <p className="text-gray-500" title={post.createdAt.toLocaleString()}>
             {fromNow}
