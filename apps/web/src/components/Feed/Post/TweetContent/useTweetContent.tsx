39d0669b964a3afbca994278e613fbe6ce471aad
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-01-01 23:11:43 +0000

    feat(web): memoize content groups

--- apps/web/src/components/Feed/Post/TweetContent/useTweetContent.tsx
+++ apps/web/src/components/Feed/Post/TweetContent/useTweetContent.tsx
@@ -1,28 +1,34 @@
+import { useMemo } from "react";
+
 const tagExpression = /((?:#|@)[a-z]+)/;
 
 export const useTweetContent = (content: string) => {
-  const groups = content.split(tagExpression);
+  const contentGroups = useMemo(() => {
+    const groups = content.split(tagExpression);
 
-  const groupsWithType = groups.map((word) => {
-    if (word.startsWith("#")) {
+    const groupsWithType = groups.map((word) => {
+      if (word.startsWith("#")) {
+        return {
+          type: "hashtag",
+          value: word.slice(1),
+        };
+      }
+
+      if (word.startsWith("@")) {
+        return {
+          type: "hashtag",
+          value: word.slice(1),
+        };
+      }
+
       return {
-        type: "hashtag",
-        value: word.slice(1),
+        type: "text",
+        value: word,
       };
-    }
+    });
+  }, [content]);
 
-    if (word.startsWith("@")) {
-      return {
-        type: "hashtag",
-        value: word.slice(1),
-      };
-    }
-
-    return {
-      type: "text",
-      value: word,
-    };
-  });
-
-  return groupsWithType;
+  return {
+    contentGroups,
+  };
 };
