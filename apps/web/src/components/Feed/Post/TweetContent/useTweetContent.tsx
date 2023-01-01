4be3e155cdebcac3c37960a7ea8245b6051d2c98
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-01-01 22:48:14 +0000

    feat(web): add tweet content parser logic

---
+++ apps/web/src/components/Feed/Post/TweetContent/useTweetContent.tsx
@@ -0,0 +1,26 @@
+export const useTweetContent = (content: string) => {
+  const groups = content.split(" ");
+
+  const groupsWithType = groups.map((word) => {
+    if (word.startsWith("#")) {
+      return {
+        type: "hashtag",
+        value: word.slice(1),
+      };
+    }
+
+    if (word.startsWith("@")) {
+      return {
+        type: "hashtag",
+        value: word.slice(1),
+      };
+    }
+
+    return {
+      type: "text",
+      value: word,
+    };
+  });
+
+  return groupsWithType;
+};
