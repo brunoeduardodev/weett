b3d70d386a9ebb9fd8564d26cb604102c0562f1f
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-01-02 13:11:44 +0000

    feat(web): show trending hashtags

--- apps/web/src/components/Trends/TrendsOverview.tsx
+++ apps/web/src/components/Trends/TrendsOverview.tsx
@@ -1,7 +1,21 @@
+import { trpc } from "@/utils/trpc";
+
 export const TrendsOverview = () => {
+  const { data, isLoading, isError } = trpc.hashtags.getTending.useQuery();
+
+  if (isLoading || isError) return null;
   return (
-    <section className="flex flex-col bg-gray-100 h-[420px] w-full items-center justify-center">
-      <p className="text-lg ">Coming Soon</p>
+    <section className="flex flex-col bg-gray-100 h-[420px] w-full p-4 gap-2">
+      <p className="text-lg font-bold mb-2">Trending</p>
+
+      <div className="flex flex-col gap-4">
+        {data.map((hashtag) => (
+          <div key={hashtag.id} className="flex flex-col gap-1">
+            <p className="font-bold">{hashtag.name}</p>
+            <p className="text-sm">{hashtag.count} Weets</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
