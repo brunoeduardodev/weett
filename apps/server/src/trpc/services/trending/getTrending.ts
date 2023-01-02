362dd89d13dde194b754765ba25ed67a2b36874f
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-01-02 13:04:24 +0000

    feat: add getTrending service

---
+++ apps/server/src/trpc/services/trending/getTrending.ts
@@ -0,0 +1,34 @@
+import { Context } from "../../context";
+
+export const getTrending = async ({ prisma }: Context) => {
+  const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+
+  const yesterday = new Date(new Date().getTime() - ONE_DAY_IN_MS);
+
+  const topUsages = await prisma.hashtagUsage.groupBy({
+    by: ["hashtagId"],
+    _count: { hashtagId: true },
+    where: { usedAt: { gt: yesterday } },
+    orderBy: { _count: { hashtagId: "desc" } },
+  });
+
+  const hashtags = await prisma.hashtag.findMany({
+    where: { id: { in: topUsages.map((usage) => usage.hashtagId) } },
+  });
+
+  const orderedHashtagsWithCount = topUsages
+    .map((usage) => {
+      const hashtag = hashtags.find(
+        (hashtag) => hashtag.id === usage.hashtagId
+      );
+      if (!hashtag) throw new Error("Found usage without creation");
+
+      return {
+        count: usage._count.hashtagId,
+        ...hashtag,
+      };
+    })
+    .sort((a, b) => a.count - b.count);
+
+  return orderedHashtagsWithCount;
+};
