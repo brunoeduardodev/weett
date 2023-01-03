6f9685420e09363125a66661eef8bb9c666744fb
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-01-03 15:20:29 +0000

    feat(web): make trends overview clickable

--- apps/web/src/components/Trends/TrendsOverview.tsx
+++ apps/web/src/components/Trends/TrendsOverview.tsx
@@ -1,19 +1,24 @@
 import { trpc } from "@/utils/trpc";
+import Link from "next/link";
 
 export const TrendsOverview = () => {
   const { data, isLoading, isError } = trpc.hashtags.getTending.useQuery({});
 
   if (isLoading || isError) return null;
   return (
-    <section className="flex flex-col bg-gray-100 min-h-[420px] w-full p-4 gap-2">
-      <p className="text-lg font-bold mb-2">Trending</p>
+    <section className="flex flex-col bg-gray-100 min-h-[420px] pt-4 w-full">
+      <p className="text-lg font-bold mb-2 px-4">Trending</p>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col">
         {data.map((hashtag) => (
-          <div key={hashtag.id} className="flex flex-col gap-1">
+          <Link
+            href={`/hashtags/${encodeURIComponent(hashtag.name.slice(1))}`}
+            key={hashtag.id}
+            className="flex flex-col gap-1 hover:bg-gray-200 transition-colors px-4 p-2"
+          >
             <p className="font-bold">{hashtag.name}</p>
             <p className="text-sm">{hashtag.count} Weets</p>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
