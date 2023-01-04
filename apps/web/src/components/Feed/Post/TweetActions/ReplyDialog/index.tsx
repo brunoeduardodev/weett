43484c65db9ded2fa22c54e88588e4a066e01c59
brunoeduardodev <brunoeduardo.dev@gmail.com>
2023-01-04 13:32:17 +0000

    feat(web): implement zodForm to post reply

--- apps/web/src/components/Feed/Post/TweetActions/ReplyDialog/index.tsx
+++ apps/web/src/components/Feed/Post/TweetActions/ReplyDialog/index.tsx
@@ -1,10 +1,13 @@
 import { consts } from "@/config/consts";
 import { useBody } from "@/hooks/useBody";
+import { useZodForm } from "@/hooks/useZodForm";
 import { trpc } from "@/utils/trpc";
 import * as Dialog from "@radix-ui/react-dialog";
 import { inferRouterOutputs } from "@trpc/server";
+import { ReplyPostInput, replyPostSchema } from "@weett/schemas";
 import { AppRouter } from "@weett/server";
 import { Button } from "@weett/ui";
+import { useCallback } from "react";
 import { PostPreview } from "./PostPreview";
 
 type Props = {
@@ -12,12 +15,20 @@ type Props = {
 
   isOpen: boolean;
   onClose: () => void;
-  onSubmit: (content: string) => void;
 };
 
-export const ReplyDialog = ({ isOpen, onClose, onSubmit, post }: Props) => {
+export const ReplyDialog = ({ isOpen, onClose, post }: Props) => {
   const { data: currentUser, isLoading, error } = trpc.user.me.useQuery();
   const body = useBody();
+
+  const { register, handleSubmit } = useZodForm({
+    schema: replyPostSchema,
+    defaultValues: { postId: post.id },
+  });
+
+  const onSubmit = useCallback((input: ReplyPostInput) => {
+    console.log({ input });
+  }, []);
 
   if (!currentUser || isLoading || error) return null;
 
@@ -29,20 +40,27 @@ export const ReplyDialog = ({ isOpen, onClose, onSubmit, post }: Props) => {
         <Dialog.Content className="bg-white rounded-sm shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] max-h-[85vh] max-w-xl p-6 animate-modal-open flex flex-col z-50">
           <div className="flex flex-col">
             <PostPreview post={post} />
-            <div className="flex gap-3 w-full">
-              <img
-                src={currentUser.profile.avatarUrl || consts.DEFAULT_PROFILE}
-                alt="Your Avatar"
-                className="w-12 h-12 rounded-full"
-              />
+            <form
+              className="flex flex-col gap-2"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="flex gap-3 w-full">
+                <img
+                  src={currentUser.profile.avatarUrl || consts.DEFAULT_PROFILE}
+                  alt="Your Avatar"
+                  className="w-12 h-12 rounded-full"
+                />
 
-              <textarea
-                className="w-full mt-2 p-2"
-                placeholder="Weet your reply"
-              ></textarea>
-            </div>
-
-            <Button className="self-end mt-2 rounded-full">Send</Button>
+                <textarea
+                  className="w-full mt-2 p-2"
+                  placeholder="Weet your reply"
+                  {...register("content")}
+                />
+              </div>
+              <Button className="self-end rounded-full" type="submit">
+                Send
+              </Button>
+            </form>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
